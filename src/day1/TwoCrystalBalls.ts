d0972ba58447ad75eb91dd941340c1e784dc038d
Martossy Alex <github@mail.martossy.hu>
2023-05-13 13:11:09 +0200

    sqrt deez nuts

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,13 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const squirt = Math.floor(Math.sqrt(breaks.length));
+    for (let i = squirt-1; i < breaks.length; i += squirt) {
+        if (breaks[i]) {
+            for (let j = i - squirt; j <= i; j++) {
+                if (breaks[j]) {
+                    return j;
+                }
+            }
+        }
+    }
+    return -1;
 }
