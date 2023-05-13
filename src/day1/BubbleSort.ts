fb18430af34894b28374ca3ab05666ee1670d35b
Martossy Alex <github@mail.martossy.hu>
2023-05-13 13:25:53 +0200

    bubble teabag

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,12 @@
 export default function bubble_sort(arr: number[]): void {
-
+    let end = arr.length;
+    for (; end > 0; end--) {
+        for (let i = 1; i < end; i++) {
+            if (arr[i-1] > arr[i]) {
+                const nuts = arr[i];
+                arr[i] = arr[i-1];
+                arr[i-1] = nuts;
+            }
+        }
+    }
 }
