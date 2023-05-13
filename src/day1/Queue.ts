15a7234487448f5c73e602b64686f0be0f32d006
Martossy Alex <github@mail.martossy.hu>
2023-05-13 14:28:16 +0200

    queue my arrayiasm

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,27 @@
 export default class Queue<T> {
     public length: number;
 
-    
+    private readonly array: T[];
 
     constructor() {
+        this.length = 0;
+        this.array = [];
     }
 
     enqueue(item: T): void {
+        this.length = this.array.unshift(item);
+    }
 
-}
     deque(): T | undefined {
+        const val = this.array.pop();
+        this.length = this.array.length;
+        return val;
+    }
 
-}
     peek(): T | undefined {
-
-}
+        if (this.length < 1) {
+            return undefined;
+        }
+        return this.array[this.length - 1];
+    }
 }
