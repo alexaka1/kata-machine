6784e7caf5d5dde41cf539c840bb2e5bf3652611
Martossy Alex <github@mail.martossy.hu>
2023-05-13 18:30:05 +0200

    linked list fail 2

--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,18 +1,20 @@
+type Node<T> = { value: T; next?: Node<T>; };
 export default class SinglyLinkedList<T> {
     public length: number;
-    private head: LinkedElement<T>;
-    private tail: LinkedElement<T>;
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
         this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     prepend(item: T): void {
-        // this is the only time this.head is accessed and could actually be undefined, despite TS not complaining, but that is actually the value we want
-        this.head = {value: item, next: this.head};
-        if (this.length === 0) {
-            this.tail = this.head;
+        const node: Node<T> = {value: item, next: this.head};
+        if (!this.head) {
+            this.tail = node;
         }
+        this.head = node;
         this.length++;
     }
 
@@ -25,27 +27,25 @@ export default class SinglyLinkedList<T> {
             this.append(item);
             return;
         }
+        this.length++;
         // worst case
-        let current: LinkedElement<T> = this.head;
-        for (let i = 1; i < this.length; i++) {
-            const temp = current.next;
-            if (temp == null) {
-                current.next = {value: item, next: undefined};
-                return;
-            }
+        let current = this.head;
+        let next: Node<T> | undefined = current;
+        for (let i = 1; i <= idx && current; i++) {
+            next = current?.next;
             if (i === idx) {
-                const next = temp.next;
+                const next: Node<T> = {value: item, next: current?.next};
                 current.next = {next: next, value: item};
             }
-            current = temp;
+            current = next;
         }
     }
 
     append(item: T): void {
-        const next: LinkedElement<T> = {value: item, next: undefined};
-        if (this.length === 0) {
-            this.head = next;
-            this.tail = next;
+        this.length++;
+        const next: Node<T> = {value: item, next: undefined};
+        if (!this.tail) {
+            this.tail = this.head = next;
         } else {
             this.tail.next = next;
             this.tail = next;
@@ -56,15 +56,15 @@ export default class SinglyLinkedList<T> {
         if (this.length === 0) {
             return undefined;
         }
-        if (this.head.value === item) {
+        if (this.head?.value === item) {
             return this.removeAt(0);
         }
         let prev = this.head;
-        let current = this.head.next;
+        let current = this.head?.next;
         while (current != null) {
             if (current.value === item) {
                 const value = current.value;
-                prev.next = current.next;
+                prev!.next = current.next;
                 this.length--;
                 return value;
             }
@@ -80,24 +80,17 @@ export default class SinglyLinkedList<T> {
             return undefined;
         }
         if (idx === 0) {
-            return this.head.value;
+            return this.head?.value;
         }
         if (idx === this.length - 1) {
-            return this.tail.value;
+            return this.tail?.value;
         }
         // worst case
-        let current: LinkedElement<T> = this.head;
-        for (let i = 1; i < this.length; i++) {
-            const temp = current.next;
-            if (temp == null) {
-                return undefined;
-            }
-            if (i === idx) {
-                return temp.value;
-            }
-            current = temp;
+        let current = this.head;
+        for (let i = 1; i < idx; i++) {
+            current = current?.next;
         }
-        return undefined;
+        return current?.value;
     }
 
     removeAt(idx: number): T | undefined {
@@ -108,29 +101,26 @@ export default class SinglyLinkedList<T> {
             return undefined;
         }
         if (idx === 0) {
-            const value = this.head.value;
-            const next = this.head.next;
+            const value = this.head?.value;
+            const next = this.head?.next;
             if (next != null) {
                 this.head = next;
             }
             this.length--;
             return value;
         }
-        let current: LinkedElement<T> = this.head;
-        for (let i = 1; i < this.length; i++) {
-            const temp = current.next;
-            if (temp != null) {
-                if (i === idx) {
-                    const next = temp.next;
-                    current.next = temp.next;
-                    this.length--;
-                    return;
-                }
-                current = temp;
+        let current = this.head;
+        let prev = this.head;
+        for (let i = 1; i <= idx && current; i++) {
+            current = current.next;
+            if (i === idx) {
+                prev!.next = current?.next;
+                this.length--;
+                return current?.value;
             }
+            prev = current;
         }
         return undefined;
     }
 
 }
-type LinkedElement<T> = { value: T; next: LinkedElement<T> | undefined; };
