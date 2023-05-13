cab766c540e08e58795d444d976c6eb6138d7fd5
Martossy Alex <github@mail.martossy.hu>
2023-05-13 12:55:37 +0200

    binary, i hardly know 'er

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let lo = 0;
+    let hi = haystack.length;
+    do {
+        const middle = Math.floor(lo + (hi - lo) /2);
+        const daddy = haystack[middle];
+        if (daddy === needle){
+            return true;
+        }
+        if (needle < daddy){
+            hi = middle;
+        } else {
+            lo = middle + 1;
+        }
+    } while (lo < hi)
+    return false;
 }
