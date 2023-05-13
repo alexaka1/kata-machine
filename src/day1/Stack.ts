5c74ef06a3cb5ef73b2d6b298c2552fcadfb8bc2
Martossy Alex <github@mail.martossy.hu>
2023-05-13 15:47:19 +0200

    give head

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,33 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>
+}
 export default class Stack<T> {
     public length: number;
 
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        this.length++;
+        this.head = {value: item, next: this.head};
+    }
 
-}
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+        this.length--;
+        const job = this.head.value;
+        this.head = this.head.next;
+        return job;
+    }
 
-}
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
