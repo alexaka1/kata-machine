95791c2d367fdae4bcc751624f882dc6d4c1728b
Martossy Alex <github@mail.martossy.hu>
2023-05-14 16:50:09 +0200

    depth first in this ass

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,13 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(head: BinaryNode<number> | null, needle: number): boolean {
+    if (head === null) {
+        return false;
+    }
+    if (head.value === needle) {
+        return true;
+    }
+    return search(head.left, needle) || search(head.right, needle);
+}
 
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle);
 }
