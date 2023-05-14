289d14c7d7ff04004065a5c503de5f4ef174e1cc
Martossy Alex <github@mail.martossy.hu>
2023-05-14 16:40:36 +0200

    cuties

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,20 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+import {default as Queue} from "./Queue";
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = new Queue<BinaryNode<number>>();
+    q.enqueue(head);
+    while (q.length) {
+        const curr = q.deque();
+        if (curr?.value === needle) {
+            return true;
+        }
+        if (curr?.left) {
+            q.enqueue(curr?.left);
+
+        }
+        if (curr?.right) {
+            q.enqueue(curr?.right);
+        }
+    }
+    return false;
 }
